af6153a6d04928194d5907372e118253d848293b
lichy <lichunyangcoder@gmail.com>
2020-07-28 00:18:22 +0800

    header dev

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -20,7 +20,7 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-@import '../../../assets/styles/varibles.styl'
+@import '~styles/varibles.styl'
 .header
   line-height: 0.86rem;
   display: flex;
