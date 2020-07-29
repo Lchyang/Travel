8229387e5ce203fa6ff068c703b7029b8a3c1065
lichy <lichunyangcoder@gmail.com>
2020-07-29 08:35:51 +0800

    swiper component develped

---
+++ .d.ts
@@ -0,0 +1,2 @@
+// 声明vue-awesoem-swiper,没有这个文件不生效
+declare module 'vue-awesome-swiper';
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 35.25%;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
